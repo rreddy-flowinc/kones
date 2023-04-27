163a28f835238f863c356f7bb35abe4e82dfcd21
Raghu Reddy <raghu.reddy@flowinc.com>
2023-04-27 15:54:14 -0700

    Create oo code

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -1,50 +1,45 @@
 from pypdf import PdfReader
 import pdfplumber
-import pdb
-
-
 from pathlib import Path
 
-# Get the current working directory
-current_directory = Path.cwd()
-print(f"Current working directory: {current_directory}")
+class PdfExtractor:
+    def __init__(self, input_file_name):
+        self.__current_directory = Path.cwd()
+        self.__input_pdf_path = self.__current_directory / Path(f"source_pdf/{input_file_name}")
 
-# clean the output directory
-file_to_rem = Path(current_directory / "source_text/output.txt")
-if file_to_rem.exists():
-    file_to_rem.unlink()
+    def clear_files_if_exists(self):
+        file_to_rem = Path(self.__current_directory / "source_text/output.txt")
+        if file_to_rem.exists():
+            file_to_rem.unlink()
 
-# Define a relative path to a file or directory
-relative_path = Path("source_pdf/input.pdf")
-# Join the current directory with the relative path to create an absolute path
-absolute_path = current_directory / relative_path
+    def extractor_w_pypdf(self):
+        reader = PdfReader(self.__input_pdf_path)
+        number_of_pages = len(reader.pages)
 
-def extractor_w_pypdf(input_path):
-    reader = PdfReader(absolute_path)
-    number_of_pages = len(reader.pages)
-
-    # save to a text file for later use
-    # copy the path where the script and pdf is placed
-    with open((current_directory / Path("source_text/output.txt")), "x") as file1:
-        for page_num in range(number_of_pages):
-            page = reader.pages[page_num]
-            text = page.extract_text()        
-            file1.writelines(text)
-            continue
-    file1.close()
-
-def extractor_w_pdfplumber(input_path):
-    with open((current_directory / Path("source_text/output.txt")), "x") as file1:
-        with pdfplumber.open(absolute_path) as pdf:
-            page_length = len(pdf.pages)
-            for page_num in range(page_length):
-                page = pdf.pages[page_num]
-                text = page.extract_text(layout=True)
+        # save to a text file for later use
+        # copy the path where the script and pdf is placed
+        with open((self.__current_directory / Path("source_text/output.txt")), "x") as file1:
+            for page_num in range(number_of_pages):
+                page = reader.pages[page_num]
+                text = page.extract_text()        
                 file1.writelines(text)
                 continue
-    # file1.close()
+        file1.close()
+
+    def extractor_w_pdfplumber(self):
+        with open((self.__current_directory / Path("source_text/output.txt")), "x") as file1:
+            with pdfplumber.open(self.__input_pdf_path) as pdf:
+                page_length = len(pdf.pages)
+                for page_num in range(page_length):
+                    page = pdf.pages[page_num]
+                    text = page.extract_text(layout=True)
+                    file1.writelines(text)
+                    continue
+    
+    def run(self):
+        self.clear_files_if_exists()
+        self.extractor_w_pypdf()
+        # self.extractor_w_pdfplumber()
 
 
-
-# extractor_w_pypdf(absolute_path)
-extractor_w_pdfplumber(absolute_path)
+pdf = PdfExtractor('input.pdf').run()
