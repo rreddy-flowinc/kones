40882c43a1dd392b916284d95333f8d89253d3bd
Raghu Reddy <raghu.reddy@flowinc.com>
2023-04-27 15:00:42 -0700

    Try pdfplumber implementation

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -1,4 +1,5 @@
 from pypdf import PdfReader
+import pdfplumber
 import pdb
 
 
@@ -18,26 +19,32 @@ relative_path = Path("source_pdf/ms10k.pdf")
 # Join the current directory with the relative path to create an absolute path
 absolute_path = current_directory / relative_path
 
+def extractor_w_pypdf(input_path):
+    reader = PdfReader(absolute_path)
+    number_of_pages = len(reader.pages)
 
-reader = PdfReader(absolute_path)
-number_of_pages = len(reader.pages)
+    # save to a text file for later use
+    # copy the path where the script and pdf is placed
+    with open((current_directory / Path("source_text/output.txt")), "x") as file1:
+        for page_num in range(number_of_pages):
+            page = reader.pages[page_num]
+            text = page.extract_text()        
+            file1.writelines(text)
+            continue
+    file1.close()
+
+def extractor_w_pdfplumber(input_path):
+    with open((current_directory / Path("source_text/output.txt")), "x") as file1:
+        with pdfplumber.open(absolute_path) as pdf:
+            page_length = len(pdf.pages)
+            for page_num in range(page_length):
+                page = pdf.pages[page_num]
+                text = page.extract_text(layout=True)
+                file1.writelines(text)
+                continue
+    # file1.close()
 
 
 
-# save to a text file for later use
-# copy the path where the script and pdf is placed
-with open((current_directory / Path("source_text/output.txt")), "x") as file1:
-    
-    for page_num in range(number_of_pages):
-        
-        page = reader.pages[page_num]
-        text = page.extract_text()        
-        file1.writelines(text)
-        continue
-
-
-# closing the text file object
-file1.close()
-
-
-
+extractor_w_pypdf(absolute_path)
+# extractor_w_pdfplumber(absolute_path)
