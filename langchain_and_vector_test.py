c4eeaec340fd34aef11e36d1b55a4e3e6e1302c1
Raghu Reddy <raghu.reddy@flowinc.com>
2023-05-02 09:49:03 -0700

    Update vector test

--- langchain_and_vector_test.py
+++ langchain_and_vector_test.py
@@ -1,38 +1,53 @@
 import pdb
 from PyPDF2 import PdfReader
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Milvus
 
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 
-reader = PdfReader('./source_pdf/ms10k.pdf')
+from langchain.document_loaders import PyPDFLoader
+import pdfplumber
+#
 
+# from pymilvus import Milvus as pm
+#
+# host = '127.0.0.1'
+# port = '19530'
+# client = pm(host, port)
+# client.drop_collection('LangChainCollection')
+# pdb.set_trace()
+
+reader = PdfReader('./source_pdf/ms10k.pdf')
+# reader = pdfplumber.open('./source_pdf/ms10k.pdf')
 
 raw_text = ''
 for i, page in enumerate(reader.pages):
     text = page.extract_text()
     if text:
         raw_text += text
+# reader.close()
 
 
 text_splitter = CharacterTextSplitter(        
     separator = "\n",
-    chunk_size = 1000,
-    chunk_overlap  = 200,
+    chunk_size = 2000,
+    chunk_overlap  = 300,
     length_function = len,
 )
 texts = text_splitter.split_text(raw_text)
 
 
-embeddings = OpenAIEmbeddings()
 
+
+# build semantic index of documents
+embeddings = OpenAIEmbeddings()
 docsearch = Milvus.from_texts(texts, embeddings)
 
 docsearch
 
-chain = load_qa_chain(OpenAI(), chain_type="stuff")
+chain = load_qa_chain(OpenAI(model_name="gpt-3.5-turbo", temperature=0.1), chain_type="stuff")
 query = "what is microsoft's total revenue?"
 docs = docsearch.similarity_search(query)
 
@@ -40,6 +55,17 @@ docs = docsearch.similarity_search(query)
 print(chain.run(input_documents=docs, question=query))
 
 
-# query = "What was the cost of training the GPT4all model?"
-# docs = docsearch.similarity_search(query)
-# chain.run(input_documents=docs, question=query)
+query = "What was microsoft's gross profit?"
+docs = docsearch.similarity_search(query)
+print(chain.run(input_documents=docs, question=query))
+
+
+query = "Who is the CEO or Chief Executive Officer?"
+docs = docsearch.similarity_search(query)
+print(chain.run(input_documents=docs, question=query))
+
+
+
+query = "What is Microsoft's net profit margin? Net profit divided by total revenue."
+docs = docsearch.similarity_search(query)
+print(chain.run(input_documents=docs, question=query))
